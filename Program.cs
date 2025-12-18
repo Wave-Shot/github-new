510ca6455030b29d402d0aedfba0d7b62922bb33
Wave-Sh0t <vsh.2509@gmail.com>
2025-12-18 10:44:31 +0530

    Reverse

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a sentence:");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] chars = words[i].ToCharArray();
+            Array.Reverse(chars);
+            words[i] = new string(chars);
+        }
+
+        string result = string.Join(" ", words);
+        Console.WriteLine(result);
+    }
+}
